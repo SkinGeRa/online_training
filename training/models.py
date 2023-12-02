3b72acb6b52613cfee829083f89b33e333be2aae
Nickolay <psyangel93@mail.ru>
2023-12-02 14:16:44 +0300

    Update files

--- training/models.py
+++ training/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from config import settings
 from users.models import NULLABLE
 
 
@@ -28,3 +30,26 @@ class Lesson(models.Model):
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
 
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = (
+        ('CASH', 'Наличные'),
+        ('BANK_TRANSFER', 'Перевод на счет'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_date = models.DateTimeField(verbose_name='Дата оплаты', **NULLABLE)
+
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', **NULLABLE)
+
+    amount = models.PositiveSmallIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Cпособ оплаты',
+                                      default='BANK_TRANSFER')
+
+    def __str__(self):
+        return f'{self.user} {self.amount} {self.payment_method}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
--- training/serializers.py
+++ training/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from training.models import Course, Lesson
+from training.models import Course, Lesson, Payment
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -14,4 +15,26 @@ class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_set.all().count()
+
+    def get_lessons(self, course):
+        return LessonSerializer(Lesson.objects.filter(course=course), many=True).data
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
